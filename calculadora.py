8534da27165d849bdf5a4a0ab1a0eb40a9dc1560
vitorrossetto2110 <98957321+vitorrossetto2110@users.noreply.github.com>
2023-08-23 17:05:52 -0300

    calculadora python
    calculadora

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+x = int(input('digite um numero:'))
+y = int(input('digite um numero:'))
+numero1 = x
+numero2= y
+mais= x + y
+menos = x - y
+vezes= x * y
+dividir = x / y
+print(mais,menos,vezes,dividir)
