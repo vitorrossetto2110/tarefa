79ea1eee9b867ff36ad69a61180748a05a05894b
vitor2110 <75260116+vitor2110@users.noreply.github.com>
2023-09-12 20:13:33 -0300

    vetores
    vetor

---
+++ vetor.py
@@ -0,0 +1,15 @@
+doze = []
+doze.append(12)
+doze.append(6)
+doze.append(3)
+print(len(doze))
+#x = doze.pop(1)
+#doze.remove(12)
+indice = doze.index(6)
+doze.reverse()
+doze.sort()
+doze.insert(2,10)
+dez= doze
+dez[2]=15
+print(doze[2])
+onze = doze.copy()
